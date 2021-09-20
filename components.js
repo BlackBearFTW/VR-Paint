4c584cbf5c3a05f872f2341715055a013f522345
Robin Mager <robinmager@home.nl>
2021-09-20 16:07:14 +0200

    Update components.js

--- components.js
+++ components.js
@@ -3,19 +3,27 @@
 AFRAME.registerComponent('trigger-listener',{
     init: function () {
         const scene = document.querySelector("a-scene");
+        let brushPointInterval;
 
         this.el.addEventListener('triggerup', () => {
             // Clear interval
+            clearInterval(brushPointInterval);
         });
 
         this.el.addEventListener('triggerdown', () => {
+            createBrushPoint();
+            brushPointInterval = setInterval(createBrushPoint, 100);
+        });
+
+        const createBrushPoint = () => {
             const brushPoint = document.createElement("a-sphere");
+            const controllerPos = this.el.getAttribute("position");
             //brushPoint.setAttribute("geometry", "primitive: sphere");
             brushPoint.setAttribute("material", "color: blue");
-            brushPoint.setAttribute("geometry", "radius: 0.5");
-            //brushPoint.setAttribute("position", this.el.getAttribute("position"));
+            brushPoint.setAttribute("geometry", "radius: 0.05");
+            brushPoint.setAttribute("position", controllerPos);
             scene.appendChild(brushPoint);
-        });
+        }
 
         this.boxEl = document.querySelector("#box");
         this.boxEl.setAttribute("material", "color: purple");
